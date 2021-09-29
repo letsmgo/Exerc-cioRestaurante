e1c2081fa3ddd859b2265cd4749a3e3ff8008485
Let Marçal <leticiamarcalmatias@gmail.com>
2021-09-29 11:45:29 -0300

    Implementando a classe Ingredientes

---
+++ src/Ingredientes.java
@@ -0,0 +1,18 @@
+public class Ingredientes {
+    private String nomeDoIngrediente;
+
+    //Método construtor
+    public Ingredientes() {
+        this.nomeDoIngrediente = nomeDoIngrediente;
+    }
+
+    //Getters and Setters
+    public String getNomeDoIngrediente() {
+        return nomeDoIngrediente;
+    }
+
+    public void setNomeDoIngrediente(String nomeDoIngrediente) {
+        this.nomeDoIngrediente = nomeDoIngrediente;
+    }
+
+}

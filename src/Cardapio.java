1f5b0ed03a016c9aa53a4f0a1d7883b18d87a988
Let Marçal <leticiamarcalmatias@gmail.com>
2021-09-29 11:47:27 -0300

    Implementando a classe Cardapio

---
+++ src/Cardapio.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cardapio {
+    //Atributos
+    private List<Prato> pratos = new ArrayList<>();
+
+    //Método construtor
+    public Cardapio() {
+        this.pratos = pratos;
+    }
+
+    //Getters and Setters
+    public List<Prato> getPratos() {
+        return pratos;
+    }
+
+    public void setPratos(List<Prato> pratos) {
+        this.pratos = pratos;
+    }
+
+}
